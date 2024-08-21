3748fc718bee530909ebbf869e9cd8ae404bd5d6
Dzmitry Bodry <megabodry@gmail.com>
2024-08-21 14:05:38 +0300

    Completed Task 6.

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int temp = (int) numberToBeRounded;
+        if ((numberToBeRounded - temp) >= 0.5) temp++;
+        System.out.println(temp);
     }
 }
